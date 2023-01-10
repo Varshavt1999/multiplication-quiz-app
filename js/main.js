b071a9d264247a44db2ba8df01b246c8b0ce0bba
Varshavt1999 <varshavt7399@gmail.com>
2023-01-10 11:56:52 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+// parse ---> to convert string to number
+let score = JSON.parse(localStorage.getItem("score"));
+
+//while you open a new browser
+if (!score) {
+    score = 0;
+}
+
+const formEl = document.getElementById("form");
+const questionEl = document.getElementById("question");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+
+const correctAns = num1 * num2;
+
+questionEl.innerText = `what is ${num1} multiplied by  ${num2} ?`;
+
+function updateLocalStorage() {
+    localStorage.setItem("score", JSON.stringify(score));
+}
+
+formEl.addEventListener("submit", () => {
+    // +inputEl.value --> to convert inputEl.value(string) to number
+    const userAns = +inputEl.value;
+    if (userAns === correctAns) {
+        score++;
+        updateLocalStorage();
+    } else {
+        score--;
+        updateLocalStorage();
+    }
+});
+scoreEl.innerText = `score : ${score}`;
